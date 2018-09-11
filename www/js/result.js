2d8accea0d4d795a1b000b02e2724f2f093b39f7
janjira012 <s5830213012@phuket.psu.ac.th>
2018-09-11 20:57:47 +0700

    part value next page

---
+++ www/js/result.js
@@ -0,0 +1,40 @@
+//Window.SearchB = {
+
+    $(function(){
+       
+        document.write("<h5>"+localStorage.url+"</h5>")
+
+        console.log(document.write("<h5>"+localStorage.url+"</h5>"));       
+         var url = 'https://api.elsevier.com/content/search/author?query=authlast('+y+')%20and%20authfirst('+x+')%20and%20affil('+z+')&apiKey=7f59af901d2d86f78a1fd60c1bf9426a';
+         
+       //  localStorage.setItem(url);
+             // $.ajax({
+         //     url: url ,
+         //     type: 'GET',
+         //     dataType :  'json',
+             
+         //    success : function (data) {
+         //        var x = data
+         //       console.log(x['search-results']);
+         //      var c = (x['search-results'].entry[0]['preferred-name'].surname);
+         //      console.log(c);
+         //     // window.location.href='result.htm';
+             
+         //     //document.getElementsById("showresult").innerHTML = window.location.search;
+             
+         //    }
+        
+         // });
+         // console.log(url);  
+     
+    }); 
+     //}
+     
+     
+     
+             
+       
+         
+        
+     
+     
