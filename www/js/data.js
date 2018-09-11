71b6bdf78fdf6602d374bceeaa90c0f2356726fd
janjira012 <s5830213012@phuket.psu.ac.th>
2018-09-11 13:20:21 +0700

    add code new file

---
+++ www/js/data.js
@@ -0,0 +1,37 @@
+function searchB() {
+    
+    var x, text, y;
+    x = document.getElementById("searchAuthorsName").value;
+    y = document.getElementById("searchAuthorLastName").value;
+    z = document.getElementById("affiliation").value;
+
+    var url = 'https://api.elsevier.com/content/search/author?query=authlast('+y+')%20and%20authfirst('+x+')%20and%20affil('+z+')&apiKey=7f59af901d2d86f78a1fd60c1bf9426a';
+   // console.log(url);
+ 
+    $.ajax({
+        url: url ,
+        type: 'GET',
+        dataType :  'json',
+        //success : onSuccess,
+       //error : onError
+       success : function (data) {
+           var x = data
+          //console.log(data.search-results);
+          console.log(x['search-results'].entry[0]);
+
+          //window.location.href = "reult.htm?id=" ;
+       }
+    })  
+    // }).then(function (data) {
+    //     var x = data;
+    //    console.log(x.search-results);
+}  
+
+        
+        
+    //     var row = data.search-results.entry[0].eid;
+    //    console.log(row);
+       
+    
+   
+
