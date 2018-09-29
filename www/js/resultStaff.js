3a034dfbc86007b8fefa80cfcf8baefdc21943a5
janjira012 <s5830213012@phuket.psu.ac.th>
2018-09-29 15:51:10 +0700

    edit resultStaff  v1.2

--- www/js/resultStaff.js
+++ www/js/resultStaff.js
@@ -3,7 +3,7 @@ var urlSearchParams = URL.searchParams;
 let params = (new URL(document.location)).searchParams;
 let tid = params.get("id");
 var url='';
-var name,lastname,meet;
+var name,lastname,meet,issuse,total;
 $(function(){
     // is the string "id"
     
@@ -22,7 +22,7 @@ $(function(){
             // console.log(to);
             if(parseInt(jsResult["staffcoc"][i]["id"])===parseInt(tid)){
                name =jsResult["staffcoc"][i]["staffName"].toLowerCase();
-               lastname = jsResult["staffcoc"][i]["staffLastName"]..toLowerCase();
+               lastname = jsResult["staffcoc"][i]["staffLastName"].toLowerCase();
             
               r += "<b>"+ jsResult["staffcoc"][i]["position"] + "</b> <b> "+jsResult["staffcoc"][i]["staffName"]+"</b> <b>"+jsResult["staffcoc"][i]["staffLastName"]+"</b></a></br>";
               
@@ -38,7 +38,7 @@ $(function(){
 $(function(){
     var x ='https://api.elsevier.com/content/search/scopus?query=ALL(';
     var y=')&apiKey=185547eee67ed06e5e817a0f227d23fe';
-    url = x+name+'%20'+lastname+y;
+    url = x+name+'%20AND%20'+lastname+y;
     console.log(url);
     xmlhttp.open("GET", url, false);
     xmlhttp.send();
@@ -50,10 +50,27 @@ $(function(){
          var c = jsResult["search-results"]["entry"].length;
          var you = "";
          for(i =0;i<c;i++){
-           you += i+1 +". <b>"+ jsResult["search-results"]["entry"][i]["dc:title"] + "</b>,<i> "+jsResult["search-results"]["entry"][i]["prism:publicationName"]+"</i>, "+jsResult["search-results"]["entry"][i]["prism:coverDisplayDate"]+"</br>";
 
+           issuse = +jsResult["search-results"]["entry"][i]["prism:issueIdentifier"];
+           console.log(issuse);
+           if(!issuse=== issuse|| 0){
+
+            total="<h5>Number of Articles: None </h5>";
+        }
+           else{
+            total = "<h5>Number of Articles : "+parseInt(issuse)+"</h5>"; 
+           }
+          // console.log(total);
+           meet =jsResult["search-results"]["entry"][i]["link"][2]["@href"];
+           console.log(meet);
+           you += i+1 +". <b><a href="+meet+">"+ jsResult["search-results"]["entry"][i]["dc:title"] + "</a></b>,<i> "+jsResult["search-results"]["entry"][i]["prism:publicationName"]+"</i>, "+jsResult["search-results"]["entry"][i]["prism:coverDisplayDate"]+"</br>"+ "<p> Number of Citations:"+ jsResult["search-results"]["entry"][i]["citedby-count"]+"</p><br><br>";
+        
             //r += i+1 +". <b>"+ jsResult["search-results"]["entry"][i]["preferred-name"]["surname"] + "</b> ,<i>"+jsResult["search-results"]["entry"][i]["preferred-name"]["given-name"]+"</i> <i> "+jsResult["search-results"]["entry"][i]["affiliation-current"]["affiliation-name"]+"</i>,"+jsResult["search-results"]["entry"][i]["document-count"]+"</br>";
          }
          document.getElementById("showresult").innerHTML = you;
+         document.getElementById("NumberofArticles").innerHTML = total;
+    }else {
+        var text = "none" ;
+        document.getElementById("showresult").innerHTML = text;
     }
 })
